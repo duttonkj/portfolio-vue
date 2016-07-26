d37aa4b7a37f83e25657bb7d79b6cec110d74340
Blast On <duttonkj@gmail.com>
2016-07-25 22:03:29 -0400

    nav rough in

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,39 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-    <!-- dyanmic router view -->
-   <router-view
-     class="view"
-     keep-alive
-     transition
-     transition-mode="out-in">
-   </router-view>
+    <site-navigation>
+      <div class="site-navigation__rail">
+        <site-navigation-item link="/about" color="green">About</site-navigation-item>
+        <site-navigation-item link="/work" color="orange">Work</site-navigation-item>
+        <site-navigation-item link="/outside-work" color="blue">Outside of Work</site-navigation-item>
+      </div>
+      <div class="site-navigation__rail site-navigation__rail--right">
+        <site-navigation-item link="/endorsements" color="purple">Endorsements</site-navigation-item>
+        <site-navigation-item link="/recognition" color="purple">Recognition</site-navigation-item>
+        <site-navigation-item link="/lets-talk" color="purple">Let's Talk</site-navigation-item>
+      </div>
+      <site-navigation-item link="/" color="link" home-link="true">Home</site-navigation-item>
+    </site-navigation>
+    <main class="content">
+     <router-view
+       class="view"
+       keep-alive
+       transition
+       transition-mode="out-in">
+     </router-view>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import SiteNavigation from './components/site-navigation'
+import SiteNavigationItem from './components/site-navigation-item'
+
+export default {
+  components: {
+    SiteNavigation,
+    SiteNavigationItem
+  }
+}
 </script>
 
 <style>
---
+++ src/components/site-navigation-item.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="site-navigation__item" v-bind:class="[
+    'site-navigation__item--' + color,
+    {
+      'site-navigation__item--home': homeLink
+    }
+  ]" >
+    <a v-link="link" class="site-navigation__link"><slot></slot></a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['link', 'color', 'homeLink'],
+  data () {
+    return {
+      orange: 'site-navigation__item--orange',
+      green: 'site-navigation__item--green',
+      blue: 'site-navigation__item--blue',
+      purple: 'site-navigation__item--purple',
+      ocean: 'site-navigation__item--ocean',
+      red: 'site-navigation__item--red'
+    }
+  },
+  computed: {
+    home () {
+      return 'home-class'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
