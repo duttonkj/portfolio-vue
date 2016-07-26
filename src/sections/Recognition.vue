35b84f79091b1ddcb5e5e0cc09c7c4ed456a41d9
Blast On <duttonkj@gmail.com>
2016-07-26 10:39:07 -0400

    recognition content

--- src/sections/Recognition.vue
+++ src/sections/Recognition.vue
@@ -1,26 +1,40 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="section-animator">
+    <wrap-section id="recognition" title="Recognition" color="ocean">
+      <block-base>
+        <block-header>
+          <block-title>Superman Award</block-title>
+        </block-header>
+        <block-content>
+          <block-text>
+            <mark>Recognized</mark> with the Superman Award for overall embodiment of company values and someone who keeps things on track, rolls up sleeves when necessary, and goes to battle for the good of the team.
+          </block-text>
+          <block-text>
+            <img class="award__image match__image" src="https://s3.amazonaws.com/blaston-llc/superman-award-lowfi.jpg" />
+          </block-text>
+        </block-content>
+      </block-base>
+    </wrap-section>
   </div>
 </template>
 
 <script>
+// Grab all of block templates for use
+import BlocksMixin from './../mixins/blocks'
+import WrapSection from './../components/wrap-section'
+
 export default {
+  mixins: [BlocksMixin],
   data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Recognition View!'
-    }
+    return {}
+  },
+  components: {
+    WrapSection
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
-  color: #42b983;
-}
+
 </style>
